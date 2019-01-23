a8a808ab3dac67e84c3047b2d4b982f1073b1497
Andrew Wilson <46463678+abwilson2020@users.noreply.github.com>
2019-01-22 23:06:02 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  consumer_key:         'oFMEBfi68XXgXBINhdXqSd9OS',
-  consumer_secret:      'DgdYnfxtbD8U7N2onUZMPNcasVtJ2oUBa2OzSuv5QzXfAxWLLx',
-  access_token:         '1037404426334547974-itUE3xBXiMn4KmFKzYZe9237R9rt2G',
-  access_token_secret:  'EM72y9cHVNuvnRsfpqLmrmaNnpJ1pa1JvW6raRE0IS40g',
+  consumer_key:         'insert consumer_key here',
+  consumer_secret:      'insert consumer_secret here',
+  access_token:         'insert access_token here',
+  access_token_secret:  'insert access_token_secret here',
 }
